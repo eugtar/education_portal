da251f3b50f592321020a02b08fb01e851e8ccdb
Eugene Tarabanov <tarabanov96@gmail.com>
2024-11-09 03:51:01 +0000

    replace ICollection with IEnumerable, reassign collections using null! value

--- Domain/Entities/EBook.cs
+++ Domain/Entities/EBook.cs
@@ -9,5 +9,4 @@ public partial class Ebook : Material
     public required DateTime PublishedOn { get; set; }
     public required int FormatId { get; set; }
     public virtual Format Format { get; set; } = null!;
-    // public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
 }
--- Domain/Entities/Skill.cs
+++ Domain/Entities/Skill.cs
@@ -5,6 +5,6 @@ namespace Domain.Entities;
 public partial class Skill : BaseEntity
 {
     public required string Name { get; set; } = null!;
-    public virtual ICollection<UserSkill> UserSkills { get; set; } = [];
-    public virtual ICollection<Course> Courses { get; set; } = [];
+    public virtual IEnumerable<UserSkill> UserSkills { get; set; } = null!;
+    public virtual IEnumerable<Course> Courses { get; set; } = null!;
 }
